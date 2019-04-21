e1ba9da7546ab93883a467feaeb3320ba0b30b0c
Sara Vieira <hey@iamsaravieira.com>
2019-04-21 12:18:22 +0200

    clea clors

--- packages/app/src/app/components/Preview/DevTools/Tests/TestDetails/index.tsx
+++ packages/app/src/app/components/Preview/DevTools/Tests/TestDetails/index.tsx
@@ -20,7 +20,6 @@ import TestBlock from './TestBlock';
 import ErrorDetails from './ErrorDetails';
 import TestSummaryText from '../TestSummaryText';
 import TestProgressBar from '../TestProgressBar';
-import theme from '@codesandbox/common/lib/theme';
 
 type Props = {
   file: File;
